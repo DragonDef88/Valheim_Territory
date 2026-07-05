fa77796c69561bfd9949f3a19bbec777b55bc4ff
DragonDef88 <infernal_def@hotmail.com>
2026-07-06 00:30:02 +0300

    Wire runtime pipeline coordinator

--- Source/ClanTerritory/Features/Runtime/RuntimeModule.cs
+++ Source/ClanTerritory/Features/Runtime/RuntimeModule.cs
@@ -1,14 +1,14 @@
-﻿using ClanTerritory.Abstractions;
+﻿using System;
+using ClanTerritory.Abstractions;
 using ClanTerritory.Core;
 using ClanTerritory.Events;
 using ClanTerritory.Features.Runtime.Events;
 using ClanTerritory.Features.Runtime.Orchestration;
+using ClanTerritory.Features.Runtime.Pipeline;
 using ClanTerritory.Features.Runtime.Services;
 using ClanTerritory.Features.Territory.Services;
 using ClanTerritory.Features.WorldDiscovery.Services;
 using ClanTerritory.Utils;
-using System;
-
 
 namespace ClanTerritory.Features.Runtime
 {
@@ -16,6 +16,8 @@ namespace ClanTerritory.Features.Runtime
     {
         private RuntimeStateMachine _stateMachine;
         private IRuntimeInitializationService _runtimeInitializationService;
+        private RuntimePipeline _runtimePipeline;
+        private RuntimePipelineCoordinator _runtimePipelineCoordinator;
 
         public void Initialize()
         {
@@ -25,10 +27,23 @@ namespace ClanTerritory.Features.Runtime
                     "EventBus is not registered.");
             }
 
-            _stateMachine = new RuntimeStateMachine(eventBus); ServiceContainer.Register(_stateMachine);
+            _stateMachine = new RuntimeStateMachine(eventBus);
+            ServiceContainer.Register(_stateMachine);
 
             _runtimeInitializationService = new RuntimeInitializationService();
-            ServiceContainer.Register<IRuntimeInitializationService>(_runtimeInitializationService);
+            ServiceContainer.Register<IRuntimeInitializationService>(
+                _runtimeInitializationService);
+
+            _runtimePipeline = new RuntimePipeline();
+            ServiceContainer.Register(_runtimePipeline);
+
+            _runtimePipelineCoordinator =
+                new RuntimePipelineCoordinator(_runtimePipeline);
+
+            ServiceContainer.Register(_runtimePipelineCoordinator);
+
+            eventBus.Subscribe<RuntimeStateChangedEvent>(
+                _runtimePipelineCoordinator);
 
             if (!ServiceContainer.TryGet<IWorldDiscoveryService>(
                     out IWorldDiscoveryService worldDiscoveryService))
@@ -37,16 +52,16 @@ namespace ClanTerritory.Features.Runtime
                     "WorldDiscoveryService is not registered.");
             }
 
-            ITerritoryService territoryService;
-
-            if (!ServiceContainer.TryGet(out territoryService))
+            if (!ServiceContainer.TryGet<ITerritoryService>(
+                    out ITerritoryService territoryService))
             {
                 throw new InvalidOperationException(
                     "TerritoryService is not registered.");
             }
-            RuntimeOrchestrator orchestrator =new RuntimeOrchestrator(
-                _stateMachine, 
-                worldDiscoveryService, 
+
+            RuntimeOrchestrator orchestrator = new RuntimeOrchestrator(
+                _stateMachine,
+                worldDiscoveryService,
                 territoryService);
 
             ServiceContainer.Register<IRuntimeOrchestrator>(orchestrator);
@@ -54,7 +69,6 @@ namespace ClanTerritory.Features.Runtime
             eventBus.Subscribe<RuntimeStateChangedEvent>(orchestrator);
 
             ModLog.Info("Runtime module initialized.");
-        
         }
 
         public void Shutdown()
