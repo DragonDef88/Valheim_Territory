5031140336c009e4e90a44f9da97a9e03e0f1e00
DragonDef88 <infernal_def@hotmail.com>
2026-07-08 17:03:05 +0300

    Add Jotunn dependency

--- Source/ClanTerritory/Core/Plugin.cs
+++ Source/ClanTerritory/Core/Plugin.cs
@@ -3,6 +3,7 @@
 namespace ClanTerritory.Core
 {
     [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
+    [BepInDependency("com.jotunn.jotunn", BepInDependency.DependencyFlags.HardDependency)]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
